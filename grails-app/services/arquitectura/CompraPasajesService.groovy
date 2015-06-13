ecba692fcd9e7241e83baf95c3e386c44590dddb
felizarazol <felizarazol@unal.edu.co>
2015-06-12 22:54:35 -0500

    Cambio en los servicios segun recomendaciones Ajustes de configuracion

--- grails-app/services/arquitectura/CompraPasajesService.groovy
+++ grails-app/services/arquitectura/CompraPasajesService.groovy
@@ -9,60 +9,59 @@ import javax.xml.ws.*
 import javax.xml.bind.annotation.*
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter
 
-@Transactional
+@WebService (targetNamespace="http://Flotas/")
+@SOAPBinding(parameterStyle=SOAPBinding.ParameterStyle.BARE)
 class CompraPasajesService {
     
     static expose = ['cxfjax']
     
-    //Retorna todos los viajes disponibles
-    @WebMethod(operationName = "ConsultarViajes",action="http://terminal/ConsultarViajes")
-    def Pasaje ConsultasViajes(){
-        return Pasaje.list()
-    }
-    
-    //Retorna true si existe la cantidad de pasajes que quiere el usuario
-    @WebMethod(operationName = "TiquetesDisponibles",action="http://terminal/TiquetesDisponibles")
-    def boolean TiquetesDisponibles(@WebParam(name='idPasaje') String idPasaje, @WebParam(name='numeroComprados') int numeroComprados) {
-        def pasaje = Pasaje.findByIdPasaje(idPasaje)
-        if (pasaje != null){
-            if (pasaje.asiento > numeroComprados){
-                return true
-            }
-        }
-        return false
-    }
-    
     //crea el usuario en esta aplicacion y vende los pasajes solicitados
-    @WebMethod(operationName = "ComprarTiquetes",action="http://terminal/ComprarTiquetes")
-    def boolean ComprarTiquetes(@WebParam(name='usuario') String usuario, @WebParam(name='nombre') String nombre, @WebParam(name='apellido') String apellido, 
-        @WebParam(name='email') String email, @WebParam(name='contrasena') String contrasena, @WebParam(name='idVenta') String idVenta,
-        @WebParam(name='placa') String placa, @WebParam(name='empresa') String empresa, @WebParam(name='origen') String origen, @WebParam(name='destino') String destino,
-        @WebParam(name='numeroCompra') int numeroCompra, @WebParam(name='viaje') String viaje, @WebParam(name='compra') String compra,
-        @WebParam(name='precioT') int precioT, @WebParam(name='iden') int iden){
-        //Registro del nuevo usuario
-        def u = new Usuario (nombreUsuario: usuario, 
-                            nombre: nombre, 
-                            apellido: apellido, 
-                            correo: email, 
-                            contraseña: contrasena, 
-                            rol: "usuario")
-        //Valida el registro del nuevo usuario
-        if (!u.validate()) return false
+    @WebMethod(operationName = "ComprarTiquetes",action="http://Flotas/ComprarTiquetes")
+    def boolean ComprarTiquetes(@WebParam(name='usuario') String usuario){
         //Compra de los tiquetes
         def c = new Usuario_Pasaje(nombreUsuario: usuario, 
-                            idPasaje: idVenta, 
-                            placaBus: placa, 
-                            empresa: empresa,
-                            origen: origen,
-                            destino: destino,
-                            numeroComprados: numerosCompra,
-                            fechaViaje: viaje,
-                            fechaVenta: compra,
-                            precio: precioT,
-                            id: iden)
+                            idPasaje: /*Cambiar*/, 
+                            placaBus: /*Cambiar*/, 
+                            empresa: /*Cambiar*/,
+                            origen: /*Cambiar*/,
+                            destino: /*Cambiar*/,
+                            numeroComprados: 1,
+                            precio: /*Cambiar*/,
+                            id: /*Cambiar*/)
         //Valida el registro de la nueva venta
         if(!c.validate()) return false
-        //Si las dos se cumplen se retorna true
+        //Si las dos se cumplen se guarda y se retorna true
+        c.save(flush:true)
         return true
+    }
+    
+    @WebMethod(operationName = "CrearUsuario",action="http://terminal/CrearUsuario")
+    def boolean CrearUsuario(@WebParam(name='usuario') String usuario, @WebParam(name='nombre') String nombre,
+        @WebParam(name='apellido') String apellido, @WebParam(name='email') String email, @WebParam(name='password') String password){
+        def u = new Usuario (nombreUsuario: usuario, 
+                    nombre: nombre, 
+                    apellido: apellido, 
+                    correo: email, 
+                    contraseña: password, 
+                    rol: "usuario")
+        //Valida el registro del nuevo usuario
+        if (!u.validate()){
+            return false
+        }
+        u.save(flush:true)
+        return true
+    }
+    
+    //Se consulta si el usuario existe o no en el sistema
+    @WebMethod(operationName = "ExisteUsuario",action="http://terminal/ExisteUsuario")
+    def boolean ExisteUsuario(@WebParam(name='user') String user, @WebParam(name='password') String password){
+        def consult = Usuario.findByNombreUsuario(user)
+            if(consult==null){
+                return false
+            }
+            if(consult.contraseña != password){
+                return false
+            }
+            return true
     }
 }
